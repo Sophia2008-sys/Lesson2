b80e603eaf2fbdc481cd0c035e9f70eedb71f2c0
Sophia <horohovama56@gmail.com>
2022-05-14 11:33:08 +0300

    Lesson2 14.05 start

--- lesson3.py
+++ lesson3.py
@@ -2,9 +2,25 @@ class Monitor:
     displayResolution = 0
     screenSize = 0
     rate = 0
-    def __init__(self):
+    def __init__(self,px,dm,Hz):
         print("Create new Monitor")
+        self.displayResolution = px
+        self.screenSize = dm
+        self.rate = Hz
+    def printInfo(self,name):
+        print("f{name:*10}")
+        print("DisplayResolution", lg.displayResolution)
+        print("Screen Size", lg.screenSize)
+        print("Image refresh rate", lg.rate)
+        def test(self):
+            print"test"
+class Phone:
 
-lg = Monitor()
-philips = Monitor()
+
+lg = Monitor(1500,500,1000)
+philips = Monitor(500, 345, 6789)
+lg.printInfo("LG")
+philips.printInfo("PHILIPS")
+print("LG")
+
 

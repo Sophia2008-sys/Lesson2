884c575ad15d16f1906b4d91bd49d299dfbf0a6c
Sophia <horohovama56@gmail.com>
2022-05-19 14:39:27 +0300

    Lesson2 14.05 start cat

---
+++ homework3.py
@@ -0,0 +1,18 @@
+class cat:
+    hp = 100
+    eat = 100
+    name = "amur"
+def __init__(self,n):
+    self.name = n
+    print(self.name,"hi")
+    self.life()
+    self.live()
+    self.eating()
+def eating(self):
+    self.eat += 15
+    print(self.eat)
+def live(self):
+    self.eat -=5
+def damage(self):
+    self.ho -=30
+amur = ("Amur")

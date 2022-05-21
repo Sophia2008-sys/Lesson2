3112d0b9551614dcb21b6869f24d8b1b1e48cf08
Sophia <horohovama56@gmail.com>
2022-05-21 10:45:54 +0300

    Lesson2 14.05 start cat

---
+++ lesson5.py
@@ -0,0 +1,11 @@
+class Bird:
+    color = 0
+    def fly():
+        pass
+class Flamingo(Bird):
+    migration = True
+    def standOfOneLef(self):
+        pass
+class Sparow(Bird):
+    def jumping(self):
+        pass

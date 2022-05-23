f41fe5dd1aba1f3589038e6eb489c4e674bdd33f
Sophia <horohovama56@gmail.com>
2022-05-23 09:51:13 +0300

    homework4

---
+++ homework4.py
@@ -0,0 +1,18 @@
+class soda:
+    fizzywater = 0
+    lemonade = 0
+def __init__(self,f,l):
+    self.fizzywater = f
+    self.lemonade = l
+def add(self):
+    self.f += 1
+    self.l = 0
+    print(self.f)
+    print(self.l)
+def show_my_drink(self):
+    if self.f > 0:
+        print("«Газована вода з {ДОБАВКА}»")
+    else:
+        print("«Звичайна газована вода»")
+        show_my_drink(self)
+        print(show_my_drink ,self)

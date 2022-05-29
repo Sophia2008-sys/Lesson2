44308056559e3b51ecb075f6bf07ed6995efcfb4
Sophia <horohovama56@gmail.com>
2022-05-29 14:32:43 +0300

    try - except else finally create exception

---
+++ homework6.py
@@ -0,0 +1,11 @@
+class primaryschool:
+    def read (self):
+        pass
+class secondaryschool(primaryschool):
+    def count(self):
+        pass
+    def write(self):
+        pass
+class highschool(primaryschool , secondaryschool):
+    def learn (self):
+        pass
